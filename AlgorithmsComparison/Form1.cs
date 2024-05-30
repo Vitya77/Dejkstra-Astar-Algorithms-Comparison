d0ab1c67ab4fc1e0a7e3df5ca0f90f4aabfabc1f
IvanFranCOCK <49644012+Vitya77@users.noreply.github.com>
2024-05-30 22:11:01 +0300

    Add generating of the graph

--- AlgorithmsComparison/Form1.cs
+++ AlgorithmsComparison/Form1.cs
@@ -12,9 +12,63 @@ namespace AlgorithmsComparison
 {
     public partial class Form1 : Form
     {
+        private Graph.Graph currentGraph;
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Paint += PictureBox_Paint;
+            pictureBox2.Paint += PictureBox_Paint;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numberOfVertices = int.Parse(textBox1.Text);
+            int numberOfEdges = int.Parse(textBox2.Text);
+            currentGraph = new Graph.Graph(numberOfVertices, numberOfEdges, pictureBox1.Height, pictureBox1.Width);
+            pictureBox1.Invalidate();
+            pictureBox2.Invalidate();
+        }
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (currentGraph != null)
+            {
+                DrawGraph(e.Graphics, currentGraph);
+            }
+        }
+
+        private void DrawGraph(Graphics graphics, Graph.Graph graph)
+        {
+            int numberOfVertices = graph.vertices.Length;
+
+            
+
+            // Малювання ребер
+            Pen edgePen = new Pen(Color.Black);
+            for (int i = 0; i < numberOfVertices; i++)
+            {
+                for (int j = 0; j < numberOfVertices; j++)
+                {
+                    if (graph.adjacencyMatrix[i, j] > 0)
+                    {
+                        graphics.DrawLine(edgePen, graph.vertices[i], graph.vertices[j]);
+                    }
+                }
+            }
+
+            // Малювання вершин
+            Brush vertexBrush = new SolidBrush(Color.Red);
+            Font font = new Font("Arial", 9);
+            Brush textBrush = new SolidBrush(Color.White);
+            for (int i = 0; i < graph.vertices.Length; i++)
+            {
+                graphics.FillEllipse(vertexBrush, graph.vertices[i].X - 7, graph.vertices[i].Y - 7, 14, 14);
+
+                // Малювання номера вершини
+                string vertexNumber = i.ToString();
+                SizeF stringSize = graphics.MeasureString(vertexNumber, font);
+                graphics.DrawString(vertexNumber, font, textBrush, graph.vertices[i].X - stringSize.Width / 2, graph.vertices[i].Y - stringSize.Height / 2);
+            }
         }
     }
 }
---
+++ AlgorithmsComparison/Graph.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Graph
+{ 
+    public class Graph
+    {
+        public readonly double[,] adjacencyMatrix;
+        public readonly Point[] vertices;
+
+        public Graph(int numberOfVertices, int numberOfEdges, int height, int width)
+        {
+            if (numberOfEdges > numberOfVertices * (numberOfVertices - 1) / 2)
+            {
+                throw new ArgumentOutOfRangeException("Кількість ребер не може перевищувати максимальну n*(n-1)/2, де n - кількість вершин");
+            }
+
+            Point[] Vertices = new Point[numberOfVertices];
+
+            Random random = new Random();
+
+            for (int i = 0; i < numberOfVertices; i++)
+            {
+                Vertices[i] = new Point(random.Next(width), random.Next(height));
+            }
+
+            vertices = Vertices;
+
+            double[,] AdjacencyMatrix = new double[numberOfVertices, numberOfVertices];
+
+            for (int i = 0; i < numberOfEdges; i++)
+            {
+                int source = random.Next(numberOfVertices);
+                int destination = -1;
+                while (destination == -1 || destination == source)
+                {
+                    destination = random.Next(numberOfVertices);
+                }
+
+                if (AdjacencyMatrix[source, destination] == 0)
+                {
+                    AdjacencyMatrix[source, destination] = Math.Sqrt(Math.Pow(vertices[source].X - vertices[destination].X, 2) + Math.Pow(vertices[source].Y - vertices[destination].Y, 2));
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            adjacencyMatrix = AdjacencyMatrix;
+        }
+    }
+
+}
